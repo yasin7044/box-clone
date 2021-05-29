449adbbe8e31340ddc96d10c2f87727e5534df17
yasin_codelogicx2020 <yasin@codelogicx.com>
2021-05-29 13:38:20 +0530

    Change to Box Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    <Box />
+    <div class="BoxContainer">
+      <Box
+        v-for="(eachBox, index) in box"
+        :key="index"
+        :index="index"
+        :eachBox="eachBox"
+        @changeCurrentBoxColour="changeCurrentBoxColour"
+      />
+    </div>
   </div>
 </template>
 
@@ -12,5 +20,45 @@ export default {
   components: {
     Box,
   },
+  data() {
+    return {
+      box: [],
+    };
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+    this.generateBox();
+  },
+  methods: {
+    generateBox() {
+      for (let i = 0; i < 50; i++) {
+        this.box.push(this.getRandomColor());
+      }
+    },
+    handleScroll() {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        this.generateBox();
+      }
+    },
+    getRandomColor() {
+      const color = Math.floor(Math.random() * 16777215).toString(16);
+      return "#" + color;
+    },
+    changeCurrentBoxColour(index, $event) {
+      console.log($event);
+      this.$set(this.box, index, this.getRandomColor());
+    },
+  },
 };
 </script>
+<style scoped>
+.BoxContainer {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: left;
+  align-items: stretch;
+}
+</style>
--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,64 +1,25 @@
 <template>
   <div class="BoxContainer">
+    <!-- {{ eachBox }} -->
     <div
       class="Box"
       :style="{ 'background-color': eachBox }"
-      v-for="(eachBox, index) in box"
-      :key="index"
-      @click="changeCurrentBox(index)"
+      @click="$emit('changeCurrentBoxColour', index)"
     ></div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "Box",
-  data() {
-    return {
-      box: [],
-    };
-  },
-  destroyed() {
-    window.removeEventListener("scroll", this.handleScroll);
-  },
-  created() {
-    window.addEventListener("scroll", this.handleScroll);
-    this.generateBox();
-  },
-  methods: {
-    generateBox() {
-      for (let i = 0; i < 30; i++) {
-        this.box.push(this.getRandomColor());
-      }
-    },
-    handleScroll() {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        this.generateBox();
-      }
-    },
-    getRandomColor() {
-      const color = Math.floor(Math.random() * 16777215).toString(16);
-
-      return "#" + color;
-    },
-    changeCurrentBox(index) {
-      this.$set(this.box, index, this.getRandomColor());
-    },
+  props: {
+    eachBox: {},
+    index: {},
   },
 };
 </script>
 <style scoped>
-.BoxContainer {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: left;
-  align-items: center;
-  flex-grow: 1;
-  margin: 30px;
-}
 .Box {
-  width: 13em;
-  height: 13em;
+  width: 164px;
+  height: 164px;
   border: 2px solid black;
 }
 </style>
